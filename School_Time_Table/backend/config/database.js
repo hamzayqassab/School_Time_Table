0e1917b2bf100b8572cb5a8c3db4bb38c55a07d4
hamzayqassab <91833723+hamzayqassab@users.noreply.github.com>
2025-11-15 07:27:21 +0200

    Update database.js

--- School_Time_Table/backend/config/database.js
+++ School_Time_Table/backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // Railway provides MONGODB_URL, not MONGODB_URI
+    
     const mongoURI = process.env.MONGODB_URL || process.env.MONGODB_URI;
 
     if (!mongoURI) {
@@ -11,11 +11,12 @@ const connectDB = async () => {
       );
     }
     await mongoose.connect(mongoURI);
-    console.log("✅ MongoDB Connected");
+    console.log(" MongoDB Connected");
   } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error);
+    console.error(" MongoDB Connection Error:", error);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
