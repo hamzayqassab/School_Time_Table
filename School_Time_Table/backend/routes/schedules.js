c4828489d46db06844389941a419c93fa961caef
hamzayqassab <91833723+hamzayqassab@users.noreply.github.com>
2025-11-15 08:04:44 +0200

    Refactor schedules.js for consistency

--- School_Time_Table/backend/routes/schedules.js
+++ School_Time_Table/backend/routes/schedules.js
@@ -13,9 +13,6 @@ router
   .delete(scheduleController.deleteSchedule);
 router.patch("/:id", scheduleController.updateSchedule);
 
-// POST /api/v1/schedules/shuffle
 router.post("/shuffle", scheduleController.shuffleSchedules);
 
 module.exports = router;
-
-
